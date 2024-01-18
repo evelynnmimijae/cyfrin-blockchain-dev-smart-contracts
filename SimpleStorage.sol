21f12912a75f54e63155d45e6e309a218838b11b
Evelynn Jones <39884663+evelynnmimijae@users.noreply.github.com>
2024-01-18 00:35:18 +0000

    Learned about Solidity types, declaring variables, bits, and bytes.

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,5 +2,7 @@
 pragma solidity 0.8.19; // stating our version - use 0.8.19
 
 contract SimpleStorage {
-    
+    // Basic Types: boolean, uint, int, address, bytes. Explored different Solidity types. See Notes and/or Docs file for deets and review. 
+    // Now, favorite number gets initialized at 0 if no value is given.
+    uint256 FavoriteNumber; // 0
 }
