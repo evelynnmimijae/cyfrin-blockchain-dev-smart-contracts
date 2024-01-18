a473bf1fac67e7a741b4861b44a0e97a6b4713d5
Evelynn Jones <39884663+evelynnmimijae@users.noreply.github.com>
2024-01-18 01:03:40 +0000

    Deployed a contract that allows user to state one favorite number on the blockchain. The contract holds the number, but users can access or update the number using the "store" function.

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,5 +4,10 @@ pragma solidity 0.8.19; // stating our version - use 0.8.19
 contract SimpleStorage {
     // Basic Types: boolean, uint, int, address, bytes. Explored different Solidity types. See Notes and/or Docs file for deets and review. 
     // Now, favorite number gets initialized at 0 if no value is given.
+    
     uint256 FavoriteNumber; // 0
+
+        function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+        // Successfully deployed SimpleStorage contract to address 0xd9145CCE52D386f254917e481eB44e9943F39138 using a fake environment through Remix VM (Merge) 
 }
